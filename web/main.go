bed588549d4129adad4f35fabcbe75a67d152c2a
brianbianchi <bianchibrianj@gmail.com>
2023-11-09 22:26:37 -0500

    Refactor

--- web/main.go
+++ web/main.go
@@ -31,66 +31,15 @@ func main() {
 		urlCode := strings.TrimPrefix(r.URL.Path, "/")
 		pageData := PageData{Code: urlCode}
 
-		if r.Method == "GET" {
+		switch r.Method {
+		case http.MethodGet:
 			serveForm(w, db, pageData)
-			return
-		}
 
-		if r.Method == "POST" {
-			err := r.ParseForm()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
+		case http.MethodPost:
+			handleFormSubmission(w, r, db, pageData)
 
-			email := r.FormValue("email")
-			links := r.Form["links"]
-
-			if urlCode == "" {
-				existingCode, _ := data.GetUserByEmail(db, email)
-				if existingCode != "" {
-					pageData.Error = "Your email is already registered."
-					serveForm(w, db, pageData)
-					return
-				}
-				newUserCode, err := data.CreateUser(db, email)
-				if err != nil {
-					pageData.Error = "Failed to create a new user."
-					serveForm(w, db, pageData)
-					return
-				}
-				err = data.CreateSubs(db, links, newUserCode)
-				if err != nil {
-					pageData.Error = "Failed to create new subs."
-					serveForm(w, db, pageData)
-					return
-				}
-				pageData.Code = newUserCode
-				pageData.SuccessCreated = true
-				serveForm(w, db, pageData)
-			}
-			if urlCode != "" {
-				email, _ := data.GetUserByCode(db, urlCode)
-				if email == "" {
-					pageData.Error = "Your link isn't correct."
-					serveForm(w, db, pageData)
-					return
-				}
-				err = data.DeleteSubs(db, urlCode)
-				if err != nil {
-					pageData.Error = "Failed to delete subs."
-					serveForm(w, db, pageData)
-					return
-				}
-				err = data.CreateSubs(db, links, urlCode)
-				if err != nil {
-					pageData.Error = "Failed to create subs."
-					serveForm(w, db, pageData)
-					return
-				}
-				pageData.SuccessUpdated = true
-				serveForm(w, db, pageData)
-			}
+		default:
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		}
 	})
 
@@ -108,6 +57,76 @@ func unsubscribeHandler(db *sql.DB) http.HandlerFunc {
 	}
 }
 
+func handleFormSubmission(w http.ResponseWriter, r *http.Request, db *sql.DB, pageData PageData) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	email := r.FormValue("email")
+	links := r.Form["links"]
+
+	if pageData.Code == "" {
+		handleNewUser(w, db, email, links, pageData)
+	} else {
+		handleExistingUser(w, db, links, pageData)
+	}
+}
+
+func handleNewUser(w http.ResponseWriter, db *sql.DB, email string, links []string, pageData PageData) {
+	existingCode, _ := data.GetUserByEmail(db, email)
+	if existingCode != "" {
+		pageData.Error = "Your email is already registered."
+		serveForm(w, db, pageData)
+		return
+	}
+
+	newUserCode, err := data.CreateUser(db, email)
+	if err != nil {
+		pageData.Error = "Failed to create a new user."
+		serveForm(w, db, pageData)
+		return
+	}
+
+	err = data.CreateSubs(db, links, newUserCode)
+	if err != nil {
+		pageData.Error = "Failed to create new subs."
+		serveForm(w, db, pageData)
+		return
+	}
+
+	pageData.Code = newUserCode
+	pageData.SuccessCreated = true
+	serveForm(w, db, pageData)
+}
+
+func handleExistingUser(w http.ResponseWriter, db *sql.DB, links []string, pageData PageData) {
+	email, _ := data.GetUserByCode(db, pageData.Code)
+	if email == "" {
+		pageData.Error = "Your link isn't correct."
+		serveForm(w, db, pageData)
+		return
+	}
+
+	err := data.DeleteSubs(db, pageData.Code)
+	if err != nil {
+		pageData.Error = "Failed to delete subs."
+		serveForm(w, db, pageData)
+		return
+	}
+
+	err = data.CreateSubs(db, links, pageData.Code)
+	if err != nil {
+		pageData.Error = "Failed to create subs."
+		serveForm(w, db, pageData)
+		return
+	}
+
+	pageData.SuccessUpdated = true
+	serveForm(w, db, pageData)
+}
+
 func serveForm(w http.ResponseWriter, db *sql.DB, pageData PageData) {
 	path := util.GetRootPath()
 	template, err := template.ParseFiles(fmt.Sprint(path, "web/index.html"))
@@ -115,16 +134,17 @@ func serveForm(w http.ResponseWriter, db *sql.DB, pageData PageData) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	pageData, err = getPageData(db, pageData)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	err = template.Execute(w, pageData)
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
 }
 
 func getPageData(db *sql.DB, pageData PageData) (PageData, error) {
@@ -132,20 +152,24 @@ func getPageData(db *sql.DB, pageData PageData) (PageData, error) {
 	if err != nil {
 		return pageData, err
 	}
+
 	subs := make(map[data.Feed]bool)
 	for _, f := range feeds {
 		subs[f] = false
 	}
+
 	if pageData.Code != "" {
 		email, err := data.GetUserByCode(db, pageData.Code)
 		if err != nil {
 			return pageData, err
 		}
+
 		pageData.Email = email
 		userSub, err := data.GetSubs(db, pageData.Code)
 		if err != nil {
 			return pageData, err
 		}
+
 		for _, link := range userSub {
 			for key := range subs {
 				if key.Link == link {
